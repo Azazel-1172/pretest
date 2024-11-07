9dd562fffbe047b2d6a25203d55f5853aff7bc68
MSI\gordo <gordon70705@gmail.com>
2024-11-07 16:04:58 +0800

    feat(PWA): add PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,75 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+      },
+      manifest: {
+        name: "Pretest",
+        short_name: "Pretest",
+        theme_color: "#070809",
+        background_color: "#070809",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            purpose: "maskable",
+            src: "./public/icon.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            src: "./public/icon.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern:
+              /https:\/\/itunes\.apple\.com\/tw\/rss\/topfreeapplications/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "topfreeapps-cache",
+            },
+          },
+          {
+            urlPattern:
+              /https:\/\/itunes\.apple\.com\/tw\/rss\/topgrossingapplications/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "topgrossingapps-cache",
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(js|css|ts)/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "js-css-cache",
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
